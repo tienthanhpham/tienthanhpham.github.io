4e459855b0580fc594bd12d109bbb28a3bb5d6bf
tienthanhpham <tienthanhpham@gmail.com>
2019-11-25 17:11:08 +0700

    js Buoi8 2nd commit

--- WebFrontEnd/Buoi8_Web_8/js/script.js
+++ WebFrontEnd/Buoi8_Web_8/js/script.js
@@ -43,3 +43,66 @@ function nameSort(names) {
       if (lowerNames[j] == names[k].toLowerCase()) console.log(names[k] + "\n");
     }
 }
+
+function copyStr(str) {
+  let i = 0;
+  while (i < 10) {
+    if (i == 9) {
+      document.write(str);
+      return;
+    }
+    document.write(str + "-");
+    i++;
+  }
+}
+
+function factorial(numb) {
+  let fact = 1;
+  if (numb < 0) return -1;
+  if (numb == 0) {
+    fact = 1;
+  } else {
+    for (let i = numb; i > 0; i--) fact *= i;
+  }
+  return fact;
+}
+
+function stringReverse(str) {
+  let reversed = [];
+  let revStr = "";
+  reversed = str.split("");
+  for (let i = str.length - 1; i >= 0; i--) revStr += str[i];
+  return revStr;
+}
+
+function nameSortIndex(names) {
+  let lowerNames = [];
+  for (let i = 0; i < names.length; i++) {
+    lowerNames[i] = names[i].toLowerCase();
+  }
+  lowerNames.sort();
+  for (let j = 0; j < lowerNames.length; j++)
+    for (let k = 0; k < names.length; k++) {
+      if (lowerNames[j] == names[k].toLowerCase())
+        console.log(k + ", " + names[k] + "\n");
+    }
+}
+
+function remainder(numbers) {
+  let remainderArr = [];
+  for (let i = 0; i < numbers.length; i++) {
+    remainderArr[i] = numbers[i] % 2;
+  }
+  return remainderArr;
+}
+
+function writeNumber() {
+  let i = 1;
+  let j = 2;
+  while (i <= 100) {
+    document.write('<span style="color:red" >' + i + " " + "</span>");
+    document.write('<span style="color:green" >' + j + " " + "</span>");
+    i = i + 2;
+    j = j + 2;
+  }
+}
