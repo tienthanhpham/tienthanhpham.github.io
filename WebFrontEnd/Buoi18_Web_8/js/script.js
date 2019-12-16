4e688907ad9036a0a83541c53bc61d718ae6f62d
tienthanhpham <tienthanhpham@gmail.com>
2019-12-16 17:24:42 +0700

    jquery-todolist 2

--- WebFrontEnd/Buoi18_Web_8/js/script.js
+++ WebFrontEnd/Buoi18_Web_8/js/script.js
@@ -1,107 +1,107 @@
-$(document).ready(function() {
-  let todolist = [];
-  let ul = $("<ul>");
-  let display = ul.appendTo("body");
-  let index;
-  let answer;
-  let id = -1;
+// $(document).ready(function() {
+let todolist = [];
+let ul = $("<ul>");
+let display = ul.appendTo("body");
+let index;
+let answer;
+let id = -1;
 
+render(todolist);
+
+function render(lists) {
+  let itemlist = lists
+    .map(
+      item =>
+        `<li id="${item.id}"><div>${item.value} 
+        <div class="minus" onclick = "confirm(this)"><i class="fas fa-minus" ></i></div>
+        <div class="edit" onclick = "editItem(this)"><i class="fas fa-pencil-alt"></i></div></div></li>`
+    )
+    .join(" ");
+
+  display.html(itemlist);
+  display.addClass("listUI");
+  let buttonInput = $("#submit");
+  buttonInput.click(addItem);
+
+  $("#iteminput").val("");
+}
+
+function addItem() {
+  let itemobj = {};
+  let item = $("#iteminput");
+  if (item.val() == "") return;
+  else {
+    itemobj.id = new Date().getUTCMilliseconds();
+    itemobj.value = item.val();
+    todolist.push(itemobj);
+
+    render(todolist);
+  }
+  itemobj = {};
+}
+
+function deleteItem(id) {
+  for (let i = 0; i < todolist.length; i++) {
+    if (todolist[i].id == id) todolist.splice(i, 1);
+  }
   render(todolist);
+}
 
-  function render(lists) {
-    let itemlist = lists
-      .map(
-        item =>
-          `<li id="${item.id}"><div>${item.value} 
-        <div class="minus"><i class="fas fa-minus" ></i></div>
-        <div class="edit"><i class="fas fa-pencil-alt"></i></div></div></li>`
-      )
-      .join(" ");
+function editItem(obj) {
+  console.log("Bat dau sua");
+  $("#save").removeClass("inactive");
+  let editText = $("#iteminput");
 
-    display.html(itemlist);
-    display.addClass("listUI");
-    let buttonInput = $("#submit");
-    buttonInput.click(addItem);
-
-    $("#iteminput").val("");
-  }
-
-  function addItem() {
-    let itemobj = {};
-    let item = $("#iteminput");
-    if (item.val() == "") return;
-    else {
-      itemobj.id = new Date().getUTCMilliseconds();
-      itemobj.value = item.val();
-      todolist.push(itemobj);
-
-      render(todolist);
+  for (let i = 0; i < todolist.length; i++) {
+    if (todolist[i].id == obj.parentNode.parentNode.id) {
+      editText.val(todolist[i].value);
+      index = i;
     }
-    itemobj = {};
   }
+  return index;
+}
 
-  function deleteItem(id) {
-    for (let i = 0; i < todolist.length; i++) {
-      if (todolist[i].id == id) todolist.splice(i, 1);
-    }
-    render(todolist);
-  }
+function saveItem() {
+  let editText = $("#iteminput");
+  for (let i = 0; i < todolist.length; i++)
+    if (i == index) todolist[i].value = editText.val();
+  editText.val("");
+  render(todolist);
+  index = -1;
+}
 
-  $(".edit").click(function() {
-    console.log($(this));
-    $("#save").removeClass("inactive");
-    let editText = $("#iteminput");
+function confirm(item) {
+  console.log("Bat dau confirm");
+  $(".confirm").addClass("active"); //hien hoi thoai Confirm
+  id = $(item)
+    .parent()
+    .parent()
+    .attr("id"); //lay the li ca^n xoa
 
-    for (let i = 0; i < todolist.length; i++) {
-      if (todolist[i].id == $(this).parentNode.parentNode.id) {
-        editText.val(todolist[i].value);
-        index = i;
-      }
-    }
-    return index;
-  });
+  $(".container")
+    .children()
+    .prop("disabled", true);
+  $(".fas")
+    .parent()
+    .click(false);
+}
 
-  function saveItem() {
-    let editText = $("#iteminput");
-    for (let i = 0; i < todolist.length; i++)
-      if (i == index) todolist[i].value = editText.val();
-    editText.val("");
-    render(todolist);
-    index = -1;
-  }
+function confirmHidden() {
+  $(".fas")
+    .parent()
+    .click(confirm($(this)));
+  $(".container")
+    .children()
+    .removeAttr("disabled");
+  $(".confirm").removeClass("active");
+  $(".confirm").addClass("inactive");
+}
 
-  function confirm(item) {
-    console.log("Bat dau confirm");
-    $(".confirm").addClass("active"); //hien hoi thoai Confirm
-    id = $(item)
-      .parent()
-      .parent()
-      .attr("id"); //lay the li ca^n xoa
-
-    $(".container")
-      .children()
-      .prop("disabled", true);
-    $(".fas")
-      .parent()
-      .click(false);
-  }
-
-  function confirmHidden() {
-    $(".fas")
-      .parent()
-      .click(confirm($(this)));
-    $(".container")
-      .children()
-      .removeAttr("disabled");
-    $(".confirm").removeClass("active");
-    $(".confirm").addClass("inactive");
-  }
-
-  function getConfirm(answer) {
-    if ($(answer).text() == "YES") {
-      deleteItem(id);
-      confirmHidden();
-    } else confirmHidden();
-    id = -1;
-  }
-});
+function getConfirm(answer) {
+  if ($(answer).text() == "YES") {
+    deleteItem(id);
+    confirmHidden();
+  } else confirmHidden();
+  id = -1;
+}
+// });
