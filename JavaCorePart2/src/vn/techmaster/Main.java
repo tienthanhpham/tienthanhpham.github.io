3dee38a3609f8cdd66bcd343c9700b16b99c550d
tienthanhpham <tienthanhpham@gmail.com>
2020-09-27 22:27:52 +0700

    toller2

--- JavaCorePart2/src/vn/techmaster/Main.java
+++ JavaCorePart2/src/vn/techmaster/Main.java
@@ -6,12 +6,16 @@ import vn.techmaster.toller.PrepaidCard;
 
 public class Main {
     public static void main(String[] args) {
-//        var oneWayTicket = new OneWayTicket(180);
-//        var prepaidCard = new PrepaidCard(300);
+        var oneWayTicket = new OneWayTicket(180);
+        var prepaidCard = new PrepaidCard(200);
 
-//        Line.A.enter(oneWayTicket);
-//        Line.B.exit(oneWayTicket);
-//        System.out.println(oneWayTicket.toString());
+        Line.A.enter(oneWayTicket);
+        Line.B.exit(oneWayTicket);
+        System.out.println(oneWayTicket.toString());
+
+        Line.A.enter(prepaidCard);
+        Line.D.exit(prepaidCard);
+        System.out.println(oneWayTicket.toString());
 
     }
 }
