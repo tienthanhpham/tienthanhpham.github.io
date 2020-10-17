f2a9eed52c6fa549618406f9bf0f24d0a2d87dc0
tienthanhpham <tienthanhpham@gmail.com>
2020-10-17 18:18:03 +0700

    hotel

--- JavaCorePart2/src/vn/techmaster/Main.java
+++ JavaCorePart2/src/vn/techmaster/Main.java
@@ -2,10 +2,15 @@ package vn.techmaster;
 
 import vn.techmaster.reservation.FrontDesk;
 import java.lang.String;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        FrontDesk.run();
-
+        //FrontDesk.run();
+        Date date1 = new Date(10, 15, 2020);
+        Date date2 = new Date(10, 18, 2020);
+        Date date = new Date();
+        var dateDiff = (date2.getTime() - date1.getTime());
+        System.out.println(date.getTime()/ (24 * 60 * 60 * 1000));
     }
 }
